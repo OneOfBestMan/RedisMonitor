9825db996ec272ee7423d3baa9f00b1405e231cd
peter <1065114664@qq.com>
2018-05-04 17:53:12 +0800

    add loghelper

---
+++ RedisMonitor.Core/Log/LogHelp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisMonitor.Core.Log
+{
+    public class LogHelp
+    {
+
+
+        public static void Error(string message, Exception ex)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger("log");
+            log.Error(message, ex);
+        }
+
+        /// <summary>
+        /// 输出日志到Log4Net
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="ex"></param>
+        #region static void WriteLog(Type t, Exception ex)
+
+        public static void WriteLog(Type t, Exception ex)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Error("Error", ex);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 输出日志到Log4Net
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="msg"></param>
+        #region static void WriteLog(Type t, string msg)
+
+        public static void WriteLog(Type t, string msg)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Error(msg);
+        }
+
+        #endregion
+    }
+}
